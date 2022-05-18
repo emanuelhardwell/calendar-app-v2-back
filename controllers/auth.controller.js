9575412e292ad897376b42f420f287af8df4ec81
emanuelhardwell <emanuelnoe16@gmail.com>
2022-05-17 19:59:21 -0500

    [auth] se creo el modelo User y la funcionalidad CreateUser

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,14 +1,43 @@
 const { response } = require("express");
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/User.model");
 
 const authObject = {};
 
-authObject.createUser = (req, res = response) => {
-  const { name, email, password } = req.body;
+authObject.createUser = async (req, res = response) => {
+  const { email, password } = req.body;
 
-  res.json({
-    ok: true,
-    msg: "User created",
-  });
+  try {
+    let user = await User.findOne({ email });
+
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        msg: "Este correo ya esta en uso!!",
+      });
+    }
+
+    user = new User(req.body);
+    // encriptar el password
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(password, salt);
+
+    await user.save();
+
+    res.status(201).json({
+      ok: true,
+      msg: "User created",
+      uid: user._id,
+      name: user.name,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Comuniquese con el administrador!!",
+    });
+  }
 };
 
 authObject.loginUser = (req, res = response) => {
---
+++ models/User.model.js
@@ -0,0 +1,19 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = model("User", UserSchema);
