57ea3140e7bab333588125f7b237026d2681cc4c
emanuelhardwell <emanuelnoe16@gmail.com>
2022-05-18 20:20:46 -0500

    [Event] se agrego las funcionalidades a los controladores

--- controllers/event.controller.js
+++ controllers/event.controller.js
@@ -1,12 +1,16 @@
 const { response } = require("express");
+const Event = require("../models/Event.model");
 
 const eventObject = {};
 
-eventObject.getEvents = (req, res = response) => {
+eventObject.getEvents = async (req, res = response) => {
   try {
+    const events = await Event.find().populate("user", "name");
+
     res.status(200).json({
       ok: true,
       msg: "getEvents",
+      events,
     });
   } catch (error) {
     console.log(error);
@@ -17,11 +21,17 @@ eventObject.getEvents = (req, res = response) => {
   }
 };
 
-eventObject.createEvent = (req, res = response) => {
+eventObject.createEvent = async (req, res = response) => {
   try {
+    const newEvent = new Event(req.body);
+    newEvent.user = req.uid;
+
+    const eventSave = await newEvent.save();
+
     res.status(200).json({
       ok: true,
       msg: "createEvent",
+      event: eventSave,
     });
   } catch (error) {
     console.log(error);
@@ -32,11 +42,34 @@ eventObject.createEvent = (req, res = response) => {
   }
 };
 
-eventObject.updateEvent = (req, res = response) => {
+eventObject.updateEvent = async (req, res = response) => {
+  const id = req.params.id;
+
   try {
+    const event = await Event.findById(id);
+
+    if (!event) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Este evento no existe",
+      });
+    }
+
+    if (event.user.toJSON() !== req.uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "No tiene permisos para actualizar este evento",
+      });
+    }
+
+    const eventUpdated = await Event.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+
     res.status(200).json({
       ok: true,
       msg: "updateEvent",
+      event: eventUpdated,
     });
   } catch (error) {
     console.log(error);
@@ -47,8 +80,28 @@ eventObject.updateEvent = (req, res = response) => {
   }
 };
 
-eventObject.deleteEvent = (req, res = response) => {
+eventObject.deleteEvent = async (req, res = response) => {
+  const id = req.params.id;
+
   try {
+    const event = await Event.findById(id);
+
+    if (!event) {
+      return res.status(404).json({
+        ok: false,
+        msg: "Este evento no existe",
+      });
+    }
+
+    if (event.user.toJSON() !== req.uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "No tiene permisos para eliminar este evento",
+      });
+    }
+
+    await Event.findByIdAndDelete(id);
+
     res.status(200).json({
       ok: true,
       msg: "deleteEvent",
